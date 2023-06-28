260cc0c3b29c4343df4ba8857be08adb60d653cf
almaz <almazgimaev@gmail.com>
2023-06-28 15:26:58 +0300

    add converter

--- src/convert.py
+++ src/convert.py
@@ -1,10 +1,13 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name
-import shutil
+
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
+from tqdm import tqdm
+
+import src.settings as s
 
 
 def download_dataset(teamfiles_dir: str) -> str:
@@ -45,21 +48,99 @@ def download_dataset(teamfiles_dir: str) -> str:
     return dataset_path
 
 
-
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    objclass_bg = sly.ObjClass("background", sly.Bitmap)
+    objclass_building = sly.ObjClass("building", sly.Bitmap)
+    objclass_road = sly.ObjClass("road", sly.Bitmap)
+    objclass_water = sly.ObjClass("water", sly.Bitmap)
+    objclass_barren = sly.ObjClass("barren", sly.Bitmap)
+    objclass_forest = sly.ObjClass("forest", sly.Bitmap)
+    objclass_agriculture = sly.ObjClass("agriculture", sly.Bitmap)
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    idx_to_objclasses = {
+        1: objclass_bg,
+        2: objclass_building,
+        3: objclass_road,
+        4: objclass_water,
+        5: objclass_barren,
+        6: objclass_forest,
+        7: objclass_agriculture,
+    }
 
-    # ... some code here ...
+    tag_meta_ptg1 = sly.TagMeta("rural", sly.TagValueType.NONE)
+    tag_meta_ptg2 = sly.TagMeta("urban", sly.TagValueType.NONE)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    folder_to_tag_meta = {
+        "Rural": tag_meta_ptg1,
+        "Urban": tag_meta_ptg2,
+    }
 
-    # return project
+    teamfiles_dir = "/4import/LoveDA/"
+    rural_dirname = "Rural"
+    urban_dirname = "Urban"
+    images_dirname = "images_png"
+    masks_dirname = "masks_png"
+    dataset_path = download_dataset(teamfiles_dir)
 
+    def _create_ann(image_path, ds_path, dirname):
+        labels = []
 
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
+
+        image_name = get_file_name_with_ext(image_path)
+
+        masks_dir = os.path.join(ds_path, dirname, masks_dirname)
+        mask_path = os.path.join(masks_dir, image_name)
+        ann_np = sly.imaging.image.read(mask_path)[:, :, 2]
+        mask = ann_np != 0
+        ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+        for i in range(1, ret):
+            obj_mask = curr_mask == i
+            curr_bitmap = sly.Bitmap(obj_mask)
+            curr_obj_class = idx_to_objclasses[i]
+            if curr_bitmap.area > 100:
+                curr_label = sly.Label(curr_bitmap, curr_obj_class)
+                labels.append(curr_label)
+
+        tag = sly.Tag(meta=folder_to_tag_meta[dirname])
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=[tag])
+
+    def _process_images_and_annotations(ds, batch, ds_path, dirname, progress_cb):
+        images_names = [os.path.basename(img_name) for img_name in batch]
+
+        anns_batch = [_create_ann(image_path, ds_path, dirname) for image_path in batch]
+        img_infos = api.image.upload_paths(ds.id, images_names, batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress_cb(len(batch))
+
+    project = api.project.create(workspace_id, project_name)
+
+    meta = sly.ProjectMeta(obj_classes=[list(idx_to_objclasses.values())])
+    api.project.update_meta(project.id, meta.to_json())
+
+    ds_names = os.listdir(dataset_path)
+    for ds_name in ds_names:
+        ds_path = os.path.join(dataset_path, ds_name)
+        if os.path.isdir(ds_path):
+            dataset = api.dataset.create(project.id, ds_name)
+            rural_images_dir = os.path.join(ds_path, rural_dirname, images_dirname)
+            urban_images_dir = os.path.join(ds_path, urban_dirname, images_dirname)
+
+            rural_images = sly.fs.list_files(rural_images_dir)
+            urban_images = sly.fs.list_files(urban_images_dir)
+            all_images = rural_images + urban_images
+
+            pbar = tqdm(desc=f"Processing '{ds_name}' dataset", total=len(all_images))
+            for batch in sly.batched(rural_images, batch_size=10):
+                _process_images_and_annotations(dataset, batch, ds_path, rural_dirname, pbar.update)
+            for batch in sly.batched(urban_images, batch_size=10):
+                _process_images_and_annotations(dataset, batch, ds_path, urban_dirname, pbar.update)
+
+    return project
