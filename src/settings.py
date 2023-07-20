fa61400d8fc6c26e548b6a0c667521b20d9b1e0e
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 14:09:04 +0600

    upd custom data

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -14,11 +22,16 @@ PROJECT_NAME_FULL: str = (
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.BY_NC_SA_4_0()
-INDUSTRIES: List[Industry] = [Industry.Satellite()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.GIS()]
+CATEGORY: Category = Category.Aerial()
+
 CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
 
-RELEASE_YEAR: int = 2021
+RELEASE_DATE: Optional[str] = "2021-10-15"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://zenodo.org/record/5706578#.YZvN7SYRXdF"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -49,10 +62,22 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = {
 }
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = "https://zenodo.org/record/5706578/export/hx"
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+PAPER: Optional[
+    str
+] = "https://datasets-benchmarks-proceedings.neurips.cc/paper_files/paper/2021/file/4e732ced3463d06de0ca9a15b6153677-Paper-round2.pdf"
+CITATION_URL: Optional[str] = "https://github.com/Junjue-Wang/LoveDA#citation"
+AUTHORS: Optional[List[str]] = [
+    "Junjue Wang",
+    "Zhuo Zheng",
+    "Ailong Ma",
+    "Xiaoyan Lu",
+    "Yanfei Zhong",
+]
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Wuhan University, China"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://en.whu.edu.cn/"
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -67,10 +92,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -82,13 +112,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
